c867b65bc63b32257ff5f71a85c3a639c74b1d59
Allan Meyer <ameyer409@yahoo.com>
2024-01-12 17:46:54 -0500

    basic menu code

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -16,8 +16,8 @@ public class FilmQueryApp {
 
 	public static void main(String[] args) throws SQLException {
 		FilmQueryApp app = new FilmQueryApp();
-		app.test();
-//    app.launch();
+//		app.test();
+    app.launch();
 	}
 
 	private void test() throws SQLException {
@@ -45,7 +45,38 @@ public class FilmQueryApp {
 	}
 
 	private void startUserInterface(Scanner input) {
-
+		
+		while(true) {
+			printMainMenu();
+			System.out.println("How would you like to search?: ");
+			int option = input.nextInt();
+			switch (option) {
+//			case 1 is for looking up a film by it's ID
+			case 1:
+				System.out.println("Film by ID!");
+//				filmById();
+				break;
+//			case 2 is for looking up a film by a keyword
+			case 2:
+				System.out.println("Film by Keyword!");
+//				filmByKeyword();
+				break;
+			case 3:
+				System.out.println("Thank you for using the film lookup menu");
+				System.out.println("Goodbye!");
+				return;
+			default:
+				System.out.println("Please choose a valid menu option");
+			}
+		}
+		
+	}
+	
+	public void printMainMenu() {
+		System.out.println("------------------------------------");
+		System.out.println("----- 1: Look up a film by ID ------");
+		System.out.println("--- 2: Look up a film by keyword ---");
+		System.out.println("------------- 3: Exit --------------");
 	}
 
 }
